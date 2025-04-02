13156661a3f519dacf568d73d7263e041440fee5
Ayah Abdelhameed <a.abdelhameed@wustl.edu>
2025-04-01 19:25:49 -0500

    oass value properties to each square

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   return (
     <>
@@ -14,25 +14,26 @@ export default function App() {
 }
 
 export function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
-  )
+  );
 }
 
 
